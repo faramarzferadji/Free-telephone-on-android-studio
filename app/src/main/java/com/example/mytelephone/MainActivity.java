876b768165eabb1e710951cceb06bb6d69754ec9
faramarz <feradjif@yahoo.ca>
2021-11-17 14:23:22 -0500

    add all to master

---
+++ app/src/main/java/com/example/mytelephone/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.mytelephone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CALL = 1;
+    private EditText editTextNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextNumber = findViewById(R.id.edt_text_number);
+        ImageView imagecall = findViewById(R.id.image_call);
+        imagecall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCall();
+            }
+        });
+    }
+   private void makePhoneCall(){
+        String number = editTextNumber.getText().toString();
+        if (number.trim().length() >0){
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+
+            }
+            else {
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Enter the phone number", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL){
+          if ( grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+              makePhoneCall();
+          }
+          else {
+              Toast.makeText(this, "Permission DENIED",Toast.LENGTH_SHORT).show();
+          }
+        }
+    }
+}
